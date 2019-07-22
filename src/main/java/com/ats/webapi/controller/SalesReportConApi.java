9a05159393d287e8a3934ffb92e004ceee8fcc12
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-07-22 10:20:36 +0530

    //..neha

--- src/main/java/com/ats/webapi/controller/SalesReportConApi.java
+++ src/main/java/com/ats/webapi/controller/SalesReportConApi.java
@@ -107,4 +107,86 @@ public class SalesReportConApi {
 		return salesReportDateMonthList;
 	}
 
+	@RequestMapping(value = { "/getMonthwiseReport" }, method = RequestMethod.POST)
+	public @ResponseBody List<SalesReportDateMonth> getMonthwiseReport(@RequestParam("fromDate") String fromDate,
+			@RequestParam("toDate") String toDate, @RequestParam("frIdList") List<String> frIdList) {
+
+		List<SalesReportDateMonth> salesReportDateMonthList = new ArrayList<>();
+		List<SalesReportBillwise> salesReportBillwiseList = new ArrayList<>();
+		List<SalesReportDMCredit> grnList = null;
+		List<SalesReportDMCredit> gvnList = null;
+		try {
+			fromDate = Common.convertToYMD(fromDate);
+			toDate = Common.convertToYMD(toDate);
+
+			salesReportBillwiseList = saleReportBillwiseRepo.getSaleReportBillwiseByMonth(frIdList, fromDate, toDate);
+
+			grnList = salesReportDMCreditRepo.getDataGRNForMonth(frIdList, fromDate, toDate);
+
+			gvnList = salesReportDMCreditRepo.getDataGVNForMonth(frIdList, fromDate, toDate);
+
+			for (int i = 0; i < salesReportBillwiseList.size(); i++) {
+
+				SalesReportDateMonth save = new SalesReportDateMonth();
+
+				save.setBillDate(salesReportBillwiseList.get(i).getBillDate());
+				save.setFrId(salesReportBillwiseList.get(i).getFrId());
+				save.setGrandTotal(salesReportBillwiseList.get(i).getGrandTotal());
+				save.setTaxableAmt(salesReportBillwiseList.get(i).getTaxableAmt());
+				save.setTotalTax(salesReportBillwiseList.get(i).getTotalTax());
+				save.setMonth(salesReportBillwiseList.get(i).getMonth());
+
+				salesReportDateMonthList.add(save);
+
+			}
+
+			for (int i = 0; i < salesReportDateMonthList.size(); i++) {
+				for (int j = 0; j < grnList.size(); j++) {
+
+					if (salesReportDateMonthList.get(i).getBillDate() == grnList.get(j).getCrnDate()) {
+
+						salesReportDateMonthList.get(i).setCrnDate(grnList.get(j).getCrnDate());
+						salesReportDateMonthList.get(i).setGrnTaxableAmt(grnList.get(j).getTaxableAmt());
+						salesReportDateMonthList.get(i).setGrnTotalTax(grnList.get(j).getCrnTotalTax());
+						salesReportDateMonthList.get(i).setGrnGrandTotal(grnList.get(j).getGrandTotal());
+						break;
+
+					} else {
+
+						salesReportDateMonthList.get(i).setGrnTaxableAmt(0);
+						salesReportDateMonthList.get(i).setGrnTotalTax(0);
+						salesReportDateMonthList.get(i).setGrnGrandTotal(0);
+					}
+
+				}
+			}
+
+			for (int i = 0; i < salesReportDateMonthList.size(); i++) {
+				for (int j = 0; j < gvnList.size(); j++) {
+
+					if (salesReportDateMonthList.get(i).getBillDate() == grnList.get(j).getCrnDate()) {
+
+						salesReportDateMonthList.get(i).setCrnDate(gvnList.get(j).getCrnDate());
+						salesReportDateMonthList.get(i).setGvnTaxableAmt(gvnList.get(j).getTaxableAmt());
+						salesReportDateMonthList.get(i).setGvnTotalTax(gvnList.get(j).getCrnTotalTax());
+						salesReportDateMonthList.get(i).setGvnGrandTotal(gvnList.get(j).getGrandTotal());
+						break;
+
+					} else {
+
+						salesReportDateMonthList.get(i).setGvnTaxableAmt(0);
+						salesReportDateMonthList.get(i).setGvnTotalTax(0);
+						salesReportDateMonthList.get(i).setGvnGrandTotal(0);
+					}
+
+				}
+			}
+
+		} catch (Exception e) {
+			System.out.println(" Exce in Tax1 Report " + e.getMessage());
+			e.printStackTrace();
+		}
+		return salesReportDateMonthList;
+	}
+
 }
