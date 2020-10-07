fda82cdaae44524a5f32df95bf635c987879381a
ats-12 <ats-12@ats12>
2020-10-07 14:01:24 +0530

    //sumit

--- src/main/java/com/ats/webapi/repository/reportv2/HSNWiseReportRepo.java
+++ src/main/java/com/ats/webapi/repository/reportv2/HSNWiseReportRepo.java
@@ -10,32 +10,77 @@ import com.ats.webapi.model.reportv2.HSNWiseReport;
 
 public interface HSNWiseReportRepo extends JpaRepository<HSNWiseReport, Integer> {
 	//Mahendra----->20/09/2020------------->add sub category col. and group by sub category
-	@Query(value = "SELECT\r\n" + 
-			"    t_bill_detail.hsn_code AS id,\r\n" + 
-			"    t_bill_detail.hsn_code AS item_hsncd,\r\n" + 
-			"    t_bill_detail.sgst_per AS item_tax1,\r\n" + 
-			"    t_bill_detail.cgst_per AS item_tax2,\r\n" + 
-			"    t_bill_detail.cess_per,\r\n" + 
-			"    SUM(t_bill_detail.bill_qty) AS bill_qty,\r\n" + 
-			"    SUM(t_bill_detail.taxable_amt) AS taxable_amt,\r\n" + 
-			"    SUM(t_bill_detail.cgst_rs) AS cgst_rs,\r\n" + 
-			"    SUM(t_bill_detail.sgst_rs) AS sgst_rs,\r\n" + 
-			"    SUM(t_bill_detail.cess_rs) AS cess_rs,\r\n" + 
-			"    m_cat_sub.sub_cat_name\r\n" + 
-			"FROM\r\n" + 
-			"    t_bill_header,\r\n" + 
-			"    t_bill_detail,\r\n" + 
-			"    m_item,\r\n" + 
-			"    m_cat_sub\r\n" + 
-			"WHERE\r\n" + 
-			"    t_bill_header.bill_no = t_bill_detail.bill_no AND t_bill_header.bill_date BETWEEN :fromDate AND :toDate AND t_bill_header.del_status = 0 AND t_bill_detail.del_status = 0 AND t_bill_detail.item_id = m_item.id AND m_item.item_grp2 = m_cat_sub.sub_cat_id\r\n" + 
-			"GROUP BY\r\n" + 
-			"    item_hsncd,\r\n" + 
-			"    m_cat_sub.sub_cat_id", nativeQuery = true)
+	@Query(value = "SELECT \r\n" + 
+			"			     uuid() AS id, \r\n" + 
+			"			    t_bill_detail.hsn_code AS item_hsncd, \r\n" + 
+			"			    t_bill_detail.sgst_per AS item_tax1, \r\n" + 
+			"			    t_bill_detail.cgst_per AS item_tax2, \r\n" + 
+			"			    t_bill_detail.cess_per, \r\n" + 
+			"			    SUM(t_bill_detail.bill_qty) AS bill_qty, \r\n" + 
+			"			    SUM(t_bill_detail.taxable_amt) AS taxable_amt, \r\n" + 
+			"			    SUM(t_bill_detail.cgst_rs) AS cgst_rs, \r\n" + 
+			"			    SUM(t_bill_detail.sgst_rs) AS sgst_rs, \r\n" + 
+			"			    SUM(t_bill_detail.cess_rs) AS cess_rs, \r\n" + 
+			"			    m_cat_sub.sub_cat_name \r\n" + 
+			"			FROM \r\n" + 
+			"			    t_bill_header, \r\n" + 
+			"			    t_bill_detail, \r\n" + 
+			"			    m_item, \r\n" + 
+			"			    m_cat_sub, m_category\r\n" + 
+			"			WHERE \r\n" + 
+			"			    t_bill_header.bill_no = t_bill_detail.bill_no AND t_bill_header.bill_date BETWEEN :fromDate AND :toDate AND t_bill_header.del_status = 0 AND t_bill_detail.del_status = 0 AND t_bill_detail.item_id = m_item.id AND m_item.item_grp2 = m_cat_sub.sub_cat_id AND m_category.cat_id=m_cat_sub.cat_id and m_category.cat_id=t_bill_detail.cat_id and t_bill_detail.cat_id !=5\r\n" + 
+			"			GROUP BY \r\n" + 
+			"			    item_hsncd, \r\n" + 
+			"			    m_cat_sub.sub_cat_id\r\n" + 
+			"                \r\n" + 
+			"             UNION ALL\r\n" + 
+			"             SELECT \r\n" + 
+			"			     uuid() AS id, \r\n" + 
+			"			    t_bill_detail.hsn_code AS item_hsncd, \r\n" + 
+			"			    t_bill_detail.sgst_per AS item_tax1, \r\n" + 
+			"			    t_bill_detail.cgst_per AS item_tax2, \r\n" + 
+			"			    t_bill_detail.cess_per, \r\n" + 
+			"			    SUM(t_bill_detail.bill_qty) AS bill_qty, \r\n" + 
+			"			    SUM(t_bill_detail.taxable_amt) AS taxable_amt, \r\n" + 
+			"			    SUM(t_bill_detail.cgst_rs) AS cgst_rs, \r\n" + 
+			"			    SUM(t_bill_detail.sgst_rs) AS sgst_rs, \r\n" + 
+			"			    SUM(t_bill_detail.cess_rs) AS cess_rs, \r\n" + 
+			"			    m_cat_sub.sub_cat_name \r\n" + 
+			"			FROM \r\n" + 
+			"			    t_bill_header, \r\n" + 
+			"			    t_bill_detail, \r\n" + 
+			"			    m_sp_cake, \r\n" + 
+			"			    m_cat_sub, m_category\r\n" + 
+			"			WHERE \r\n" + 
+			"			    t_bill_header.bill_no = t_bill_detail.bill_no AND t_bill_header.bill_date BETWEEN :fromDate AND :toDate AND t_bill_header.del_status = 0 AND t_bill_detail.del_status = 0 AND t_bill_detail.item_id = m_sp_cake.sp_id  AND 5=m_cat_sub.cat_id and m_category.cat_id=5 and t_bill_detail.cat_id=5\r\n" + 
+			"			GROUP BY \r\n" + 
+			"			    item_hsncd, \r\n" + 
+			"			    m_cat_sub.sub_cat_id", nativeQuery = true)
 	List<HSNWiseReport> getReport(@Param("fromDate") String fromDate, @Param("toDate") String toDate);
 	
-	
-	
+	/*SELECT\r\n" + 
+	"    t_bill_detail.hsn_code AS id,\r\n" + 
+	"    t_bill_detail.hsn_code AS item_hsncd,\r\n" + 
+	"    t_bill_detail.sgst_per AS item_tax1,\r\n" + 
+	"    t_bill_detail.cgst_per AS item_tax2,\r\n" + 
+	"    t_bill_detail.cess_per,\r\n" + 
+	"    SUM(t_bill_detail.bill_qty) AS bill_qty,\r\n" + 
+	"    SUM(t_bill_detail.taxable_amt) AS taxable_amt,\r\n" + 
+	"    SUM(t_bill_detail.cgst_rs) AS cgst_rs,\r\n" + 
+	"    SUM(t_bill_detail.sgst_rs) AS sgst_rs,\r\n" + 
+	"    SUM(t_bill_detail.cess_rs) AS cess_rs,\r\n" + 
+	"    m_cat_sub.sub_cat_name\r\n" + 
+	"FROM\r\n" + 
+	"    t_bill_header,\r\n" + 
+	"    t_bill_detail,\r\n" + 
+	"    m_item,\r\n" + 
+	"    m_cat_sub\r\n" + 
+	"WHERE\r\n" + 
+	"    t_bill_header.bill_no = t_bill_detail.bill_no AND t_bill_header.bill_date BETWEEN :fromDate AND :toDate AND t_bill_header.del_status = 0 AND t_bill_detail.del_status = 0 AND t_bill_detail.item_id = m_item.id AND m_item.item_grp2 = m_cat_sub.sub_cat_id\r\n" + 
+	"GROUP BY\r\n" + 
+	"    item_hsncd,\r\n" + 
+	"    m_cat_sub.sub_cat_id
+*/	
 //	SELECT" + "            t_bill_detail.hsn_code as id,"
 //			+ "            t_bill_detail.hsn_code as item_hsncd," + "            t_bill_detail.sgst_per as item_tax1,"
 //			+ "             t_bill_detail.cgst_per as item_tax2,t_bill_detail.cess_per,"
@@ -101,36 +146,90 @@ public interface HSNWiseReportRepo extends JpaRepository<HSNWiseReport, Integer>
 	//Anmol----->5/12/2019------------->filter by grn or gvn
 	//Update on ---->28-09-2020 By Mahendra
 	//Desc----> add sub category col. and group by sub category
-	@Query(value = "SELECT\r\n" + 
-			"    t_credit_note_details.hsn_code AS id,\r\n" + 
-			"    t_credit_note_details.hsn_code AS item_hsncd,\r\n" + 
-			"    t_credit_note_details.sgst_per AS item_tax1,\r\n" + 
-			"    t_credit_note_details.cgst_per AS item_tax2,\r\n" + 
-			"    t_credit_note_details.cess_per,\r\n" + 
-			"    SUM(\r\n" + 
-			"        t_credit_note_details.grn_gvn_qty\r\n" + 
-			"    ) AS bill_qty,\r\n" + 
-			"    SUM(\r\n" + 
-			"        t_credit_note_details.taxable_amt\r\n" + 
-			"    ) AS taxable_amt,\r\n" + 
-			"    SUM(t_credit_note_details.cgst_rs) AS cgst_rs,\r\n" + 
-			"    SUM(t_credit_note_details.sgst_rs) AS sgst_rs,\r\n" + 
-			"    SUM(t_credit_note_details.cess_rs) AS cess_rs,\r\n" + 
-			"    m_cat_sub.sub_cat_name\r\n" + 
-			"FROM\r\n" + 
-			"    t_credit_note_details,\r\n" + 
-			"    t_credit_note_header, \r\n" + 
-			"    m_item,\r\n" + 
-			"    m_cat_sub\r\n" + 
-			"WHERE\r\n" + 
-			"    t_credit_note_header.crn_id = t_credit_note_details.crn_id AND t_credit_note_header.crn_date BETWEEN :fromDate AND :toDate AND t_credit_note_details.del_status = 0 AND t_credit_note_details.is_grn IN(:grngvnType) AND t_credit_note_details.item_id=m_item.id AND m_item.item_grp2=m_cat_sub.sub_cat_id\r\n" + 
-			"GROUP BY\r\n" + 
-			"    item_hsncd,\r\n" + 
-			"    m_cat_sub.sub_cat_id", nativeQuery = true)
+	@Query(value = "SELECT \r\n" + 
+			"			     uuid() AS id, \r\n" + 
+			"			    t_credit_note_details.hsn_code AS item_hsncd, \r\n" + 
+			"			    t_credit_note_details.sgst_per AS item_tax1, \r\n" + 
+			"			    t_credit_note_details.cgst_per AS item_tax2, \r\n" + 
+			"			    t_credit_note_details.cess_per, \r\n" + 
+			"			    SUM( \r\n" + 
+			"			        t_credit_note_details.grn_gvn_qty \r\n" + 
+			"			    ) AS bill_qty, \r\n" + 
+			"			    SUM( \r\n" + 
+			"			        t_credit_note_details.taxable_amt \r\n" + 
+			"			    ) AS taxable_amt, \r\n" + 
+			"			    SUM(t_credit_note_details.cgst_rs) AS cgst_rs, \r\n" + 
+			"			    SUM(t_credit_note_details.sgst_rs) AS sgst_rs, \r\n" + 
+			"			    SUM(t_credit_note_details.cess_rs) AS cess_rs, \r\n" + 
+			"			    m_cat_sub.sub_cat_name \r\n" + 
+			"			FROM \r\n" + 
+			"			    t_credit_note_details, \r\n" + 
+			"			    t_credit_note_header,  \r\n" + 
+			"			    m_item, \r\n" + 
+			"			    m_cat_sub ,m_category\r\n" + 
+			"			WHERE \r\n" + 
+			"			    t_credit_note_header.crn_id = t_credit_note_details.crn_id AND t_credit_note_header.crn_date BETWEEN :fromDate AND :toDate AND t_credit_note_details.del_status = 0 AND t_credit_note_details.is_grn IN(:grngvnType) AND t_credit_note_details.item_id=m_item.id AND m_item.item_grp2=m_cat_sub.sub_cat_id and m_category.cat_id=m_cat_sub.cat_id and t_credit_note_details.cat_id=m_category.cat_id and t_credit_note_details.cat_id  !=5 and t_credit_note_details.cat_id=m_category.cat_id\r\n" + 
+			"			GROUP BY \r\n" + 
+			"			    item_hsncd, \r\n" + 
+			"			    m_cat_sub.sub_cat_id\r\n" + 
+			"                UNION ALL\r\n" + 
+			"                SELECT \r\n" + 
+			"			     uuid() AS id, \r\n" + 
+			"			    t_credit_note_details.hsn_code AS item_hsncd, \r\n" + 
+			"			    t_credit_note_details.sgst_per AS item_tax1, \r\n" + 
+			"			    t_credit_note_details.cgst_per AS item_tax2, \r\n" + 
+			"			    t_credit_note_details.cess_per, \r\n" + 
+			"			    SUM( \r\n" + 
+			"			        t_credit_note_details.grn_gvn_qty \r\n" + 
+			"			    ) AS bill_qty, \r\n" + 
+			"			    SUM( \r\n" + 
+			"			        t_credit_note_details.taxable_amt \r\n" + 
+			"			    ) AS taxable_amt, \r\n" + 
+			"			    SUM(t_credit_note_details.cgst_rs) AS cgst_rs, \r\n" + 
+			"			    SUM(t_credit_note_details.sgst_rs) AS sgst_rs, \r\n" + 
+			"			    SUM(t_credit_note_details.cess_rs) AS cess_rs, \r\n" + 
+			"			    m_cat_sub.sub_cat_name \r\n" + 
+			"			FROM \r\n" + 
+			"			    t_credit_note_details, \r\n" + 
+			"			    t_credit_note_header,  \r\n" + 
+			"			    m_sp_cake, \r\n" + 
+			"			    m_cat_sub ,m_category\r\n" + 
+			"			WHERE \r\n" + 
+			"			    t_credit_note_header.crn_id = t_credit_note_details.crn_id AND t_credit_note_header.crn_date BETWEEN :fromDate AND :toDate AND t_credit_note_details.del_status = 0 AND t_credit_note_details.is_grn IN(:grngvnType) AND t_credit_note_details.item_id=m_sp_cake.sp_id  and m_category.cat_id=m_cat_sub.cat_id and t_credit_note_details.cat_id=m_category.cat_id and t_credit_note_details.cat_id  =5 \r\n" + 
+			"			GROUP BY \r\n" + 
+			"			    item_hsncd, \r\n" + 
+			"			    m_cat_sub.sub_cat_id", nativeQuery = true)
 
 	List<HSNWiseReport> getReportHsnIn(@Param("fromDate") String fromDate, @Param("toDate") String toDate, @Param("grngvnType") List<Integer> grngvnType);
 
 	
+	/*SELECT\r\n" + 
+	"    t_credit_note_details.hsn_code AS id,\r\n" + 
+	"    t_credit_note_details.hsn_code AS item_hsncd,\r\n" + 
+	"    t_credit_note_details.sgst_per AS item_tax1,\r\n" + 
+	"    t_credit_note_details.cgst_per AS item_tax2,\r\n" + 
+	"    t_credit_note_details.cess_per,\r\n" + 
+	"    SUM(\r\n" + 
+	"        t_credit_note_details.grn_gvn_qty\r\n" + 
+	"    ) AS bill_qty,\r\n" + 
+	"    SUM(\r\n" + 
+	"        t_credit_note_details.taxable_amt\r\n" + 
+	"    ) AS taxable_amt,\r\n" + 
+	"    SUM(t_credit_note_details.cgst_rs) AS cgst_rs,\r\n" + 
+	"    SUM(t_credit_note_details.sgst_rs) AS sgst_rs,\r\n" + 
+	"    SUM(t_credit_note_details.cess_rs) AS cess_rs,\r\n" + 
+	"    m_cat_sub.sub_cat_name\r\n" + 
+	"FROM\r\n" + 
+	"    t_credit_note_details,\r\n" + 
+	"    t_credit_note_header, \r\n" + 
+	"    m_item,\r\n" + 
+	"    m_cat_sub\r\n" + 
+	"WHERE\r\n" + 
+	"    t_credit_note_header.crn_id = t_credit_note_details.crn_id AND t_credit_note_header.crn_date BETWEEN :fromDate AND :toDate AND t_credit_note_details.del_status = 0 AND t_credit_note_details.is_grn IN(:grngvnType) AND t_credit_note_details.item_id=m_item.id AND m_item.item_grp2=m_cat_sub.sub_cat_id\r\n" + 
+	"GROUP BY\r\n" + 
+	"    item_hsncd,\r\n" + 
+	"    m_cat_sub.sub_cat_id
+	*/
 //	SELECT\r\n" + 
 //	"    t_credit_note_details.hsn_code AS id,\r\n" + 
 //	"    t_credit_note_details.hsn_code AS item_hsncd,\r\n" + 
